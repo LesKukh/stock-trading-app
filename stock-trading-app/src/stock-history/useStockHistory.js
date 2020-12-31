bc9858d59e949100c1e7f732862da08c2686f90e
LesKukh <73469139+LesKukh@users.noreply.github.com>
2020-12-31 11:28:14 +0200

    Update useStockHistory.js

--- stock-trading-app/src/stock-history/useStockHistory.js
+++ stock-trading-app/src/stock-history/useStockHistory.js
@@ -5,3 +5,26 @@ const zip = (keys, values) =>
         ...acc,
         [key]: values[i],
     }), {})
+
+export const useStockHistory = () => {
+    const [stockHistory, setStockHistory] = useState([]);
+
+    useEffect(() => {
+        const loadStockHistory = async () => {
+            const response = await fetch('/stock-history');
+            const fullHistory = await response.json();
+
+            const times = Object.keys(fullHistory['Time Series (30min)']);
+            const priceSnapshots = Object.values(fullHistory['Time Series (30min)'])
+                .map(snapshot => snapshot['4. close']);
+
+            setStockHistory(zip(times, priceSnapshots));
+        }
+
+        loadStockHistory();
+        const interval = setInterval(loadStockHistory, 300000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return stockHistory;
+}
